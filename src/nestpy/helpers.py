3f0806c4cb0af39628b2a5c6d863034e16833e67
Albert <k2371191@kcl.ac.uk>
2025-07-25 14:02:55 +0100

    Tweaks to helper functions

--- src/nestpy/helpers.py
+++ src/nestpy/helpers.py
@@ -55,7 +55,7 @@ def ListInteractionTypes():
 
 
 def GetInteractionObject(name):
-    '''
+    """
     This function returns the NEST interaction object for a given string.
     Parameters:
         name (str): interaction type (e.g. 'NR' or 'nr'.)
@@ -63,11 +63,11 @@ def GetInteractionObject(name):
 
     Returns:
         nestpy.INTERACTION_TYPE(Number), where Number corresponds to the string you provided.
-    '''
+    """
 
     name = name.lower()
 
-    if name == 'er':
+    if name == "er":
         raise ValueError("For 'er', specify either 'gammaray' or 'beta'")
 
     interaction_object = INTERACTION_TYPE(NEST_INTERACTION_NUMBER[name])
@@ -76,7 +76,7 @@ def GetInteractionObject(name):
 
 @np.vectorize(excluded={"nuisance_parameters", "ERYieldsParam"})
 def GetYieldsVectorized(interaction, yield_type, nc=None, detector=None, **kwargs):
-    '''
+    """
     This function calculates nc.GetYields for the various interactions and arguments we pass into it.
 
     Requires:
@@ -101,12 +101,12 @@ def GetYieldsVectorized(interaction, yield_type, nc=None, detector=None, **kwarg
     Returns:
         getattr(yield_object, yield_type) (array): array of yield values for a given yield_object (nr, etc)
         and a given yield_type (photon, electron yield as defined in parameters.)
-    '''
+    """
     if nc is None:
         # Cache the default in _NestCalcInit
-        if 'default' not in _NestCalcInit:
-            _NestCalcInit['default'] = NESTcalc(DetectorExample_XENON10())
-        nc = _NestCalcInit['default']
+        if "default" not in _NestCalcInit:
+            _NestCalcInit["default"] = NESTcalc(DetectorExample_XENON10())
+        nc = _NestCalcInit["default"]
     if type(interaction) == str:
         interaction_object = GetInteractionObject(interaction)
     else:
@@ -116,8 +116,9 @@ def GetYieldsVectorized(interaction, yield_type, nc=None, detector=None, **kwarg
     # returns the yields for the type of yield we are considering
     return getattr(yield_object, yield_type)
 
+
 def PhotonYield(**kwargs):
-    '''
+    """
     Calculates photon yield based on GetYieldsVectorized function.
 
     Parameters:
@@ -130,12 +131,12 @@ def PhotonYield(**kwargs):
     Returns:
         GetYieldsVectorized(yield_object, yield_type='PhotonYield') (array): array of yield values same dimensions as energies
 
-    '''
+    """
     return GetYieldsVectorized(yield_type="PhotonYield", **kwargs)
 
 
 def ElectronYield(**kwargs):
-    '''
+    """
     Calculates electron yield based on GetYieldsVectorized function.
 
     Parameters:
@@ -148,12 +149,12 @@ def ElectronYield(**kwargs):
     Returns:
         GetYieldsVectorized(yield_object, yield_type='ElectronYield') (array): array of yield values same dimensions as energies
 
-    '''
+    """
     return GetYieldsVectorized(yield_type="ElectronYield", **kwargs)
 
 
 def Yield(**kwargs):
-    '''
+    """
     Calculates both electron and photon yields and puts in single dictionary.
     - Useful for analysis of one interaction_type.
 
@@ -162,10 +163,10 @@ def Yield(**kwargs):
 
     Returns:
         (dict): dict with photon and electron yields arranged together by keys.
-    '''
+    """
     return {
-        'photon': PhotonYield(**kwargs),
-        'electron': ElectronYield(**kwargs),
+        "photon": PhotonYield(**kwargs),
+        "electron": ElectronYield(**kwargs),
         # What is missing?  Aren't there other parts of YieldObject?
     }
 
@@ -185,15 +186,21 @@ def get_random_position(detector, number: int):
     return np.vstack((x, y, z)).T
 
 
-def runNESTframe(
-    interaction, detector, energy, pos: list[list[float]] = None, **kwargs
+def nest_vector(
+    interaction,
+    detector,
+    energy,
+    pos: list[list[float]] = None,
+    df: bool = False,
+    **kwargs
 ):
 
     try:
+        import awkward as ak
         import pandas as pd
-    except ImportError:
-        msg = "The runNESTframe method requires the 'pandas' package.  Please install via 'pip install pandas'"
-        raise ImportError(msg)
+    except ImportError as exc:
+        msg = "The runNESTframe method requires the 'pandas' and 'awkward' package.  Please install via 'pip install pandas awkward'"
+        raise ImportError(msg) from exc
 
     interaction = GetInteractionObject(interaction)
 
@@ -202,17 +209,26 @@ def runNESTframe(
         pos = get_random_position(detector, energy.shape)
 
     # Compute the NEST outputs
-    result = array.runNESTvec(detector, interaction, energy.tolist(), pos.tolist(), **kwargs)
+    result = array.runNESTvec(
+        detector, interaction, energy.tolist(), pos.tolist(), **kwargs
+    )
 
     # Create the pandas dataframe
-    df = pd.DataFrame(
+    arr = ak.Array(
         {i: getattr(result, i) for i in result.__dir__() if not i.startswith("_")}
     )
 
     # Save truth information
-    df["energy_keV"] = energy
-    df["x_mm"] = pos[:, 0]
-    df["y_mm"] = pos[:, 1]
-    df["z_mm"] = pos[:, 2]
+    arr["energy_keV"] = energy
+    arr["x_mm"] = pos[:, 0]
+    arr["y_mm"] = pos[:, 1]
+    arr["z_mm"] = pos[:, 2]
 
-    return df
+    if df:
+        print(
+            "Making a simple dataframe,  will lose any photon timing or waveform information"
+        )
+        fields = [i for i in arr.fields if arr[i].ndim == 1]
+        arr = pd.DataFrame({i: arr[i] for i in fields})
+
+    return arr
